015e41b26b550bf9c6c7884bbe09edb067288f18
Rafal <ravmaster48@gmail.com>
2021-01-23 12:04:23 +0000

    commit2

---
+++ src/main/java/HibernateUtils.java
@@ -0,0 +1,26 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtils {
+
+    public static SessionFactory sessionFactory ;
+
+    static {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+
+        sessionFactory = new MetadataSources(registry)
+                .buildMetadata()
+                .buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+
+
+    }
+
+}
